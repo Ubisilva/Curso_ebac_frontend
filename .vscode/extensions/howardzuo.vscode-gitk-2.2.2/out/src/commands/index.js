867ff399fa4a642c3cc54d1351869ff93b3b3b9a
Ubirajara <chavedemoletas@hotmail.com>
2025-04-20 12:10:16 -0300

    Delete .vscode directory
    Exercício commitados errado.

--- .vscode/extensions/howardzuo.vscode-gitk-2.2.2/out/src/commands/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var gitk_1 = require("./gitk");
-Object.defineProperty(exports, "registryGitk", { enumerable: true, get: function () { return gitk_1.registryGitk; } });
-var gitkrepo_1 = require("./gitkrepo");
-Object.defineProperty(exports, "registryGitkrepo", { enumerable: true, get: function () { return gitkrepo_1.registryGitkrepo; } });
-//# sourceMappingURL=index.js.map
--- .vscode/extensions/howardzuo.vscode-gitk-2.2.2/out/src/template/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var gitk_1 = require("./gitk");
-Object.defineProperty(exports, "gitkHTML", { enumerable: true, get: function () { return gitk_1.gitkHTML; } });
-var gitkrepo_1 = require("./gitkrepo");
-Object.defineProperty(exports, "gitkRepoHTML", { enumerable: true, get: function () { return gitkrepo_1.gitkRepoHTML; } });
-//# sourceMappingURL=index.js.map
